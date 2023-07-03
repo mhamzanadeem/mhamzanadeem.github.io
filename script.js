d22e8b031e22a5d594f2d811e60959b0b78b78a9
mhamzanadeem <nadeemmhamza@gmail.com>
2023-07-04 02:52:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 // typed js
 
 const typed = new Typed('.multiple-text',{
-    strings: ['Blockchain Web Developer', 'Game Developer','Excel data entry specialist'],
+    strings: ['Blockchain Web Developer', 'Game Developer','Data Entry Specialist'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
