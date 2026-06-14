a4bdea87f4979cfc55bd5de033d4f4d07561034f
M Hamza Nadeem <nadeemmhamza@gmail.com>
2026-06-14 18:30:52 +0500

    Change typed text for developer role
    Updated the typed text in the script to reflect 'AI Full Stack Developer'.

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 // typed js
 
 const typed = new Typed('.multiple-text',{
-    strings: ['Flutter Development', 'Digital Marketing','AI Automation'],
+    strings: ['AI Full Stack Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
